d7674a0d5a434b57935042541c55eb4fec5e6eb4
Banje <pak2t@naver.com>
2020-05-24 02:58:12 +0900

    2020-05-24

--- 2661.c
+++ 2661.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
-int main(){
-    int a,c=1,z;
-    char b[81];
-    scanf("%d",&a);
-    while(a!=c){
-        for(z=c;z>=0;z=z-1){
-
-        }
-        c=c+1;
+char b[81];
+int d(int a,int c){
+    int z;
+    if(2*c>a){
+        printf("ERROR");
+        return 0;
     }
+    for(z=1;z<=c;z=z+1){
+        if(b[a-z]!=b[a-c-z]){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main(){
+    int a,c=1,e,z;
+    for(z=0;z<81;z=z+1){
+        b[z]='1';
+    }
+    scanf("%d",&a);
+    while(a>=c){
+        e=0;
+        for(z=1;2*z<=c;z=z+1){
+            if(d(c,z)==1){
+                e=1;
+                break;
+            }
+        }
+        if(e==0){
+            c=c+1;
+        }
+        else{
+            while(1){
+                b[c-1]=b[c-1]+1;
+                if(b[c-1]!='4'){
+                    break;
+                }
+                b[c-1]='1';
+                c=c-1;
+            }
+        }
+    }
+    b[c-1]='\0';
     printf("%s",b);
 }
